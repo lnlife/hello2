44af7e825796c88b30797d31af681d550b227f0a
lnlife <stanlogin@gmail.com>
2024-02-17 07:06:17 +0800

    df

---
+++ index.ts
@@ -0,0 +1,3 @@
+export default function hello1() {
+    return 'this is hello 1'
+}
---
+++ libs.ts
@@ -0,0 +1,7 @@
+export function hello2() {
+    return 'hh2'
+}
+
+export function hello3() {
+    return 'hh2'
+}
